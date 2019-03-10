7a6d2b9949c65a56c58027779a5dabc2c69439a9
y0lka <gabriellakondasz@gmail.com>
2019-03-10 14:35:40 +0100

    added modal

--- P2 (Memory Game)/js/app.js
+++ P2 (Memory Game)/js/app.js
@@ -2,15 +2,16 @@
 const deck = document.querySelector('.deck');
 const moves = document.querySelector('.moves');
 const resetBtn = document.querySelector('.restart');
-const minutesElement = document.querySelector('.minutes');
-const secondsElement = document.querySelector('.seconds');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
 let openedCard; //checks if open card is matching
 //let matchingCard = 0; //matches found
 let moveCounter = 0;
-let second = 0;
-let minute = 0;
-let gameState = 'start';
+let seconds = 0;
+let minutes = 0;
+let gameState = 'initial';
 let interval;
+
 
 
 // Create a list that holds all of your cards
@@ -61,6 +62,7 @@ function showCard(e) {
     openedCard = currentCard;
 
     incrementMoveCounter();
+    initTimer();
 
     // if first card is already selected
     if (firstCard !== undefined) {
@@ -76,38 +78,98 @@ function showCard(e) {
         }
         openedCard = undefined;
     }
+
+    if (gameState === 'initial') {
+        gameState = 'playing';
+    }
 }
 
 
 function incrementMoveCounter() {
     moves.innerHTML = ++moveCounter;
-
     if (moveCounter === cards.length + cards.length / 2 ||
         moveCounter === 2 * cards.length) {
-        let stars = document.querySelectorAll('.fa-star')
+        let stars = document.querySelectorAll('.fa-star');
         stars[stars.length - 1].remove();
     }
 }
 
-//function resetMoveCounter() {
-//    moves.innerHTML = '0';
-//}
 
-checkVictory = function () {
+function checkVictory() {
     var matchedCards = document.querySelectorAll('.match');
+
     if (matchedCards.length === cards.length) {
         setTimeout(function () {
+            stopTimer();
             alert("Game over");
+
         }, 500);
     }
 }
 
 
+function initTimer() {
 
+    if (gameState !== 'initial') {
+        return;
+    }
 
+    interval = setInterval(function() {
+        let normalisedSeconds = ++seconds % 60;
+
+        if (normalisedSeconds === 0) {
+            minutes++;
+        }
+
+        minutesElement.innerHTML = minutes;
+        secondsElement.innerHTML = normalisedSeconds;
+
+    }, 1000);
+}
+
+resetBtn.addEventListener('click', resetGame);
+
+function stopTimer() {
+    clearInterval(interval);
+}
+
+function resetTimer () {
+    minutesElement.innerHTML = 0;
+    secondsElement.innerHTML = 0;
+    minutes = 0;
+    seconds = 0;
+    clearInterval(interval);
+}
+
+function resetMoves() {
+    moveCounter = 0;
+    moves.innerHTML = 0;
+    let stars = document.querySelectorAll('.fa-star');
+    for (let i = 0; i < stars.length; i++) {
+    stars[i].remove();
+    }
+
+    const starsContainer = document.querySelector('.stars');
+
+    for (let i = 0; i <= 2; i++) {
+        const star = document.createElement('li');
+        const starIcon = document.createElement('i');
+        starIcon.classList.add('fa', 'fa-star');
+        star.appendChild(starIcon);
+        starsContainer.appendChild(star);
+    }
+}
+
+function resetGame() {
+    deck.innerHTML = '';
+    resetMoves();
+    resetTimer();
+    init();
+}
 
 function init() {
     cardArr = shuffle(cards);
+    gameState = 'initial';
 
     //loop over arr and add cardArr
     for (let i = 0; i < cards.length; i++) {
