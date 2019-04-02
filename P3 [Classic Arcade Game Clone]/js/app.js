fd32dd0f0806113d5f21405acb94eaa0410ab534
y0lka <gabriellakondasz@gmail.com>
2019-04-02 20:27:14 +0200

    Finished game
    added game  class:
    added modal
    added collision check function
    added reset function
    added count victory function
    added count lives function

--- P3 [Classic Arcade Game Clone]/js/app.js
+++ P3 [Classic Arcade Game Clone]/js/app.js
@@ -1,6 +1,98 @@
+
+//Game functions
+class Game {
+    constructor() {
+        this.modal = document.querySelector('#modalContainer');
+        this.scoreBoard = document.querySelector('#score');
+        this.livesBoard = document.querySelector('#lives');
+        this.victory = 0;
+        this.lives = 3;
+
+        // This listens for key presses and sends the keys to your
+        // Player.handleInput() method. You don't need to modify this.
+        document.addEventListener('keyup', function (e) {
+            if (game.victory < 5) {
+                const allowedKeys = {
+                    37: 'left',
+                    38: 'up',
+                    39: 'right',
+                    40: 'down',
+                };
+
+                player.handleInput(allowedKeys[e.keyCode]);
+            }
+        });
+    }
+    //Score counter and winning condition
+    countVictory() {
+        this.victory++;
+        this.scoreBoard.innerHTML = this.victory;
+    
+        if (this.victory === 5) {
+            setTimeout(function () {
+                //Popup modal when win
+                game.modal.style.display = 'block';
+    
+                //Closing the popup modal when we click play again or hit enter.
+                document.getElementById('button').addEventListener('click', game.closeModalAndReset);
+                document.addEventListener('keypress', game.closeModalAndReset);    
+            }, 100);
+        }
+    }
+
+    closeModalAndReset(event) {
+        if (event.type === 'click' || (event.type === 'keypress' && event.key === 'Enter')) {
+            document.querySelector('#modalContainer').style.display = 'none';
+            game.reset();
+        }
+    }
+        //Lives counter -->
+    countLives() {
+        if (this.lives > 1) {
+            this.lives--;
+            this.livesBoard.innerHTML = this.lives;
+        } else {
+            setTimeout(function () {
+                alert("YOU LOSE!");
+                //TODO -- popup
+                game.reset();
+            }, 100);
+        }
+    }
+    //--> because vehicle-player collision resets the game only after lives are lost!
+    reset() {
+        this.victory = 0;
+        this.lives = 3;
+        this.scoreBoard.innerHTML = this.victory;
+        this.livesBoard.innerHTML = this.lives;
+
+        player.setPosition(2 * grid.x, 5 * grid.y);
+    }
+    // Check collision 
+    areIntersecting(o1, o2) {
+        const o1coordonates = {
+            x0: o1.x,
+            x1: o1.x + grid.x,
+            y0: o1.y,
+            y1: o1.y + grid.y
+        }
+
+        const o2coordonates = {
+            x0: o2.x,
+            x1: o2.x + grid.x,
+            y0: o2.y,
+            y1: o2.y + grid.y
+        }
+
+        return o1coordonates.x0 < o2coordonates.x1 &&
+            o1coordonates.x1 > o2coordonates.x0 &&
+            o1coordonates.y0 < o2coordonates.y1 &&
+            o1coordonates.y1 > o2coordonates.y0;
+    }
+}
+
 // Extracted the hardcoded grid variables into a globally accessible object 
 // in order to re-use them across the application (to move our character across the grid)
-
 class Grid {
     constructor(x, y, rows, columns, verticalOffset) {
         this.x = x;
@@ -13,6 +105,7 @@ class Grid {
             yf: this.y * rows
         }
     }
+
     isWithinBounds(obj) {
         if (obj.x < this.playableArea.xi) {
             return false;
@@ -33,8 +126,6 @@ class Grid {
         return true;
     }
 }
-
-const grid = new Grid(101, 83, 6, 5, 18);
 
 // Enemies our player must avoid
 class Enemy {
@@ -61,9 +152,9 @@ class Enemy {
             this.speed = Math.max(Math.random() * 100, 25);
         }
 
-        if (areIntersecting(this, player)) {
+        if (game.areIntersecting(this, player)) {
             player.setPosition(2 * grid.x, 5 * grid.y);
-            countLives();
+            game.countLives();
         }
     }
     // Draw the enemy on the screen, required method for game
@@ -72,21 +163,19 @@ class Enemy {
     }
 }
 
-
-
-
 // Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 class Player {
     constructor(x, y) {
         this.setPosition(x, y),
-        this.sprite = 'images/char-horn-girl.png'
+            this.sprite = 'images/char-horn-girl.png'
     }
 
     setPosition(x, y) {
         this.x = x;
         this.y = y - grid.offset;
     }
-
 
     update(dt) {
 
@@ -102,7 +191,7 @@ class Player {
                 this.y = this.y - grid.y;
                 if (!grid.isWithinBounds(this)) {
                     this.y = this.y + grid.y;
-                    countVictory();
+                    game.countVictory();
                     this.setPosition(2 * grid.x, 5 * grid.y);
                 }
                 break;
@@ -130,64 +219,10 @@ class Player {
         }
     }
 }
-// This class requires an update(), render() and
-// a handleInput() method.
-
-function countVictory() {
-    const scoreBoard = document.querySelector('#score');
-    victory++;
-    scoreBoard.innerHTML = `${victory}`;
-
-    if (victory === 5) {
-        setTimeout(function () {
-            //Opening the popup modal when we click play again
-            modal.style.display = 'block';
-
-            //Closing the popup modal when we click play again  -- NOT WORKING ON ENTER!!
-            document.getElementById('button').addEventListener('click', function closeModal() {
-                document.querySelector('#modalContainer').style.display = 'none';
-                reset();
-                
-            });
-            
-        }, 100);
-        
-    }
-}
 
 
-
-function countLives() {
-    const livesBoard = document.querySelector('#lives');
-    
-    if (live > 1) {
-        live--;
-        livesBoard.innerHTML = `${live}`;
-    } else {
-        setTimeout(function () {
-            alert("YOU LOSE!");
-            reset();
-            
-            
-        }, 100);
-    }
-    
-}
-
-
-function reset() {
-    const scoreBoard = document.querySelector('#score');
-    const livesBoard = document.querySelector('#lives');
-    
-    victory = 0;
-    live = 3;
-    scoreBoard.innerHTML = `${victory}`;
-    livesBoard.innerHTML = `${live}`;
-    player.setPosition(2 * grid.x, 5 * grid.y);
-}
-
-
-
+const grid = new Grid(101, 83, 6, 5, 18);
+const game = new Game();
 // Now instantiate your objects.
 const enemy1 = new Enemy(20, 1 * grid.y, 50);
 const enemy2 = new Enemy(20, 2 * grid.y, 10);
@@ -196,50 +231,3 @@ const enemy3 = new Enemy(20, 3 * grid.y, 15);
 const allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
 const player = new Player(2 * grid.x, 5 * grid.y);
-const modal = document.querySelector('#modalContainer');
-let victory = 0;
-let live = 3;
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function (e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
-
-/*function areIntersecting(o1, o2) {
-    
-
-
-}*/
-
-
-function areIntersecting(o1, o2) {
-    const o1coordonates = {
-        x0: o1.x,
-        x1: o1.x + grid.x,
-        y0: o1.y,
-        y1: o1.y + grid.y
-    }
-
-    const o2coordonates = {
-        x0: o2.x,
-        x1: o2.x + grid.x,
-        y0: o2.y,
-        y1: o2.y + grid.y
-    }
-
-    return o1coordonates.x0 < o2coordonates.x1 &&
-        o1coordonates.x1 > o2coordonates.x0 &&
-        o1coordonates.y0 < o2coordonates.y1 &&
-        o1coordonates.y1 > o2coordonates.y0;
-}
-
-
